18b4120cc356f726d9eef8d70cf8c3787d9ee1b3
Chris Csekey <chrisjc25@gmail.com>
2017-10-13 14:45:47 -0700

    Create create_deck, shuffle, contains? methods

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,5 +1,18 @@
 defmodule Cards do
- def hello do
-   :world
+ def create_deck do
+   numbers = [1,2,3,4,5]
+   letters = ["a,","b","c"]
+
+   for number <- numbers, letter <- letters do
+     "#{number}#{letter}"
+   end
+ end
+
+ def shuffle(deck) do
+   Enum.shuffle(deck)
+ end
+
+ def contains?(deck, card) do
+   Enum.member?(deck, card)
  end
 end
