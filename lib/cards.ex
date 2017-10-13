49c2a776f0fdc791c4975f2b1747e3fc6ab3276f
Chris Csekey <chrisjc25@gmail.com>
2017-10-13 15:52:32 -0700

    Create deal, save, load methods

--- lib/cards.ex
+++ lib/cards.ex
@@ -15,4 +15,20 @@ defmodule Cards do
  def contains?(deck, card) do
    Enum.member?(deck, card)
  end
+
+ def deal(deck, hand_size) do
+   Enum.split(deck, hand_size)
+ end
+
+ def save(deck, filename) do
+   binary = :erlang.term_to_binary(deck)
+   File.write(filename, binary)
+ end
+
+ def load(filename) do
+   case File.read(filename) do
+     {:ok, binary} -> :erlang.binary_to_term binary
+     {:error, _reason} -> "File does not exist"
+   end
+ end
 end
